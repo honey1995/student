4693e463043a19f98ce728ca67d0bcb168bdeab6
honey1995 <46518082+honey1995@users.noreply.github.com>
2019-01-09 15:43:31 +0530

    newcrome

---
+++ Crome/src/java/net/url.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.net;
+
+/**
+ *
+ * @author sk
+ */
+public class url {
+
+    public static URI create(String url) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
